3ef073edd999b4d6d02a93e1f8c4e0c95a7639b2
theHumanBorch <ncborch@gmail.com>
2025-06-14 09:06:08 -0500

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A list of 8 single-cell T cell receptor sequences runs.
+#' 
+#' @description A data frame of 8 `filtered_contig_annotations.csv` files
+#' outputted from 10X Cell Ranger. The aligned data was then processed by scRepertoire
+#' and exported using the exportClones() function. More information on the 
+#' data can be found in the following [manuscript](https://pubmed.ncbi.nlm.nih.gov/39024572/).
+#'  
+#' @docType data
+#' @concept Data
+#' @name tcrpheno_clones
+#' 
+NULL
+
+
